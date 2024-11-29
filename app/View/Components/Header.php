c2caa28fcce142de0a0061e87d97abdb8f9a824a
Abdul Aliyev <aaa7c4@gmail.com>
2024-11-29 01:35:35 -0500

    Add navbar component controllers

---
+++ app/View/Components/Header.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Header extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        $navContent = [
+            'home' => [
+                'title' => 'Home',
+                'url' => '/',
+            ],
+            'jobs' => [
+                'title' => 'All jobs',
+                'url' => '/jobs',
+            ],
+            'add_job' => [
+                'title' => 'Add job',
+                'url' => '/jobs/create',
+            ],
+            'saved_jobs' => [
+                'title' => 'Saved jobs',
+                'url' => '/jobs/saved',
+            ],
+            'dashboard' => [
+                'title' => 'Dashboard',
+                'url' => '/dashboard',
+            ],
+            'sign_in' => [
+                'title' => 'Sign in',
+                'url' => '/sign-in',
+            ],
+            'sign_up' => [
+                'title' => 'Sign up',
+                'url' => '/sign-up',
+            ],
+        ];
+        return view('components.header', compact('navContent'));
+    }
+}
