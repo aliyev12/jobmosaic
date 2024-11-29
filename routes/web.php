4a50560547291ab7b6f81e7c1383749fafa21166
Abdul Aliyev <aaa7c4@gmail.com>
2024-11-29 01:00:11 -0500

    Add routes for home and jobs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Controllers\JobController;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+
+Route::resource('jobs', JobController::class);
