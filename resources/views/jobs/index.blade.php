db0940306f0f5794fdcf9d94a02aff14e8ca4de5
Abdul Aliyev <aaa7c4@gmail.com>
2024-11-30 20:15:07 -0500

    Add index, create, and show route templates

---
+++ resources/views/jobs/index.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <h1>Available Jobs</h1>
+    <ul>
+        @forelse ($jobs as $job)
+            <li>{{ $job }}</li>
+        @empty
+            <p>No jobs available</p>
+        @endforelse
+    </ul>
+</x-layout>
