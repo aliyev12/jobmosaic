b4761622a711049bc255a1417b5e55868c67d8de
Abdul Aliyev <aaa7c4@gmail.com>
2024-11-30 22:37:15 -0500

    Add global layout component

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;1,700&display=swap"
+        rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.29.2/feather.min.js"
+        integrity="sha512-zMm7+ZQ8AZr1r3W8Z8lDATkH05QG5Gm2xc6MlsCdBz9l6oE8Y7IXByMgSm/rdRQrhuHt99HAYfMljBOEZ68q5A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>{{ $title ?? 'Job Mosaic | Get Your Dream Job' }}</title>
+</head>
+
+<body class="bg-gray-300">
+    <x-header />
+    <main class="container mx-auto p-4 mt-4">
+        {{ $slot }}
+    </main>
+    <script>
+        feather.replace();
+    </script>
+</body>
+
+</html>
