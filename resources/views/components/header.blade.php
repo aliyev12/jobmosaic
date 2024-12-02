8d0b2e6d55f4875bbf4ca9b8b0e1e57a6674ad13
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-01 23:03:41 -0500

    Outfactor mobile menu to a component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -11,19 +11,8 @@
         </h1>
         <x-nav-links :navContent="$navContent" />
         <button id="hamburger" class="text-white md:hidden flex items-center">
-            <i class="fa fa-bars text-2xl"></i>
+            <i data-feather="menu"></i>
         </button>
     </div>
-
-    {{-- <nav id="mobile-menu" class="md:hidden  text-white mt-5 pb-4 space-y-2">
-        <a href={{ url($addJob['url']) }}
-            class=" bg-indigo-700 hover:bg-indigo-800 text-white px-4 py-2 rounded hover:shadow-md transition duration-300">
-            <i class="fa fa-edit"></i> {{ $addJob['title'] }}
-        </a>
-        <a href={{ url($jobs['url']) }} class="nav-link-mobile">{{ $jobs['title'] }}</a>
-        <a href={{ url($savedJobs['url']) }} class="nav-link-mobile">{{ $savedJobs['title'] }}</a>
-        <a href={{ url($dashboard['url']) }} class="nav-link-mobile">{{ $dashboard['title'] }}</a>
-        <a href={{ url($signIn['url']) }} class="nav-link-mobile">{{ $signIn['title'] }}</a>
-        <a href={{ url($signUp['url']) }} class="nav-link-mobile">{{ $signUp['title'] }}</a>
-    </nav> --}}
+    <x-nav-links :navContent="$navContent" :mobile="true" />
 </header>
