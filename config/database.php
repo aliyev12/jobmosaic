c41b884d994aa81177618ab09f89b5afe7e67928
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-02 23:33:32 -0500

    Update docker compose and local database settings

--- config/database.php
+++ config/database.php
@@ -87,9 +87,9 @@ return [
             'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'jobmosaic'),
+            'username' => env('DB_USERNAME', 'jobmosaic_admin'),
+            'password' => env('DB_PASSWORD', 'qHbUo3Vt'),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
@@ -147,7 +147,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
