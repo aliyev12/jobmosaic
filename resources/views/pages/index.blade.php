12805a7f335c35cc4adc1461518067596f105566
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-05 22:34:40 -0500

    Add show all jobs link to home

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,21 @@
 <x-layout>
-    <h1>Welcome To Job Mosaic</h1>
+    <h2 class="text-center text-3xl mb-4 font-bold border border-gray-300 p-3">Welcome To Job Mosaic</h2>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+        @forelse ($jobs as $job)
+            <x-job-card :job="$job" />
+        @empty
+            <p>No jobs available</p>
+        @endforelse
+    </div>
+
+    <div class="flex justify-center w-full">
+        <a href="{{ route('jobs.index') }}" class="flex text-xl text-center justify-center items-center">
+            <i class="mr-2" data-feather="arrow-right-circle"></i>
+            Show all jobs
+        </a>
+    </div>
+
 
     <x-bottom-banner />
 </x-layout>
