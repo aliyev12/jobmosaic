e3fa1340c4c16eb1f7d2642c2f31e16920abc2fb
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-07 23:36:20 -0500

    Implement register user

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::resource('jobs', JobController::class);
 
 Route::get('/register', [RegisterController::class, 'register'])->name('register');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.name');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/login', [LoginController::class, 'authenticate'])->name('login.authenticate');
