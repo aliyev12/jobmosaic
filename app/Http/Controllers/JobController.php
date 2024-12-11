2cde889039f0c07cd2e26b9e0f5d47564031aa14
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-10 22:08:06 -0500

    Add authorization policy for updating and deleting jobs

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -52,8 +52,7 @@ class JobController extends Controller
         ]);
 
         // Hardcoded user ID
-        $validatedData['user_id'] = 1;
-
+        $validatedData['user_id'] = auth()->user()->id;
         // Check for image
         if ($request->hasFile('company_logo')) {
             // Store the file and get path
