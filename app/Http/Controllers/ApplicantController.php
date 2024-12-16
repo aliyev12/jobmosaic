77fec2d8c3831caf9c374375c4b27547ec42565e
Abdul Aliyev <aaa7c4@gmail.com>
2024-12-15 23:22:19 -0500

    Create a controller and route for applicants

---
+++ app/Http/Controllers/ApplicantController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Applicant;
+use Illuminate\Http\Request;
+use App\Models\Job;
+use Illuminate\Http\RedirectResponse;
+
+class ApplicantController extends Controller
+{
+    // @desc Store new job application
+    // @route POST /jobs/{job}/apply
+    public function store(Request $request, Job $job): RedirectResponse
+    {
+        // Validate incoming data
+        $validatedData = $request->validate([
+            'full_name' => 'required|string',
+            'contact_phone' => 'string',
+            'contact_email' => 'required|email',
+            'message' => 'string',
+            'location' => 'string',
+            'resume' => 'required|file|mimes:pdf|max:2048',
+        ]);
+
+        // Handle resume upload
+        // TODO : swap this with uploading a resume to file share service, e.g. r2, S3 bucket, etc.
+        if ($request->hasFile('resume')) {
+            $path = $request->file('resume')->store('resumes', 'public');
+            $validatedData['resume_path'] = $path;
+        }
+
+        // Store the application
+        $application = new Applicant($validatedData);
+
+        // Add all foreign key relations
+        $application->job_id = $job->id;
+        $application->user_id = auth()->id();
+        $application->save();
+
+        return redirect()->back()->with('success', 'Your application has been submitted');
+    }
+}
--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -78,7 +78,7 @@
             <div x-cloak x-show="open" class="fixed inset-0 flex items-center justify-center bg-gray-900 bg-opacity-50">
               <div @click.away="open = false" class="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
                 <h3 class="text-lg font-semibold mb-4">Apply For {{ $job->title }}</h3>
-                <form enctype="multipart/form-data" action="">
+                <form method="POST" enctype="multipart/form-data" action="{{ route('applicant.store', $job->id) }}">
                   @csrf
                   <x-inputs.text id="full_name" name="full_name" label="Full Name" :required="true" />
                   <x-inputs.text id="contact_phone" name="contact_phone" label="Contact Phone" />
